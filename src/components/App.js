79edaab294a0db6f194dc1e37ff367aeb6ea5bc2
Rhiannon <11739072+rhiroy@users.noreply.github.com>
2020-12-09 09:43:07 -0600

    wip

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 // 👉 TASK 1 - import the axios lib from node_modules
 import axios from 'axios'
 // 👉 TASK 2 - import the contants from constants/index.js
-
+import { BASE_URL, API_KEY } from '../constants'
 import Details from './Details'
 
 export default function App() {
