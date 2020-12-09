3ca97f39feb511b38b8ff0e1983cf54f22fc79d8
Rhiannon <11739072+rhiroy@users.noreply.github.com>
2020-12-09 09:46:59 -0600

    wip

--- src/components/App.js
+++ src/components/App.js
@@ -9,7 +9,7 @@ export default function App() {
   const [friends, setFriends] = useState([]);
   const [currentFriendId, setCurrentFriendId] = useState(null);
 
-  useEffect();
+  useEffect(() => {}, []);
 
   const openDetails = (id) => {
     setCurrentFriendId(id);
